7168db8f8c30a8263d0f02daeb4d6e904765c2a8
wjkm <wjkm@mcknights.co.nz>
2026-04-13 18:35:08 +1200

    Initial commit

---
+++ 231lean.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `«231lean»` library.
+-- Import modules here that should be built as part of the library.
+import «231lean».Basic
---
+++ Main.lean
@@ -0,0 +1,4 @@
+import «231lean»
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
