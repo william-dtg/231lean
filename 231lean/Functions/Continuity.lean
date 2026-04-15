bab20f2da698b33e4966779cb942f7f098a55dd8
wjkm <wjkm@mcknights.co.nz>
2026-04-15 12:14:21 +1200

    feat: f times g continuous at c is continuous with sorry

--- 231lean/Functions/Continuity.lean
+++ 231lean/Functions/Continuity.lean
@@ -10,8 +10,6 @@ def f_is_continuous_at_a (f : ℝ → ℝ) (a : ℝ) : Prop :=
 def f_is_continuous (f : ℝ → ℝ) : Prop :=
   ∀ a, ∃ L, ε_δ_limit (Set.univ : Set ℝ) f a L 
 
-#check fun_apply
-
 -- Example proof that all constant functions are continuous
 example : 
     ∀ C, f_is_continuous (f := Function.const ℝ C) := by
@@ -95,10 +93,55 @@ theorem f_g_f_minus_g_cont_at_c
     have h1 : |f x - fl| < ε / 2 := hf x trivial ⟨x_ne_c, hδf⟩
     have h2 : |g x - gl| < ε / 2 := hg x trivial ⟨x_ne_c, hδg⟩
     have hsum : |f x - fl| + |g x - gl| < ε / 2 + ε / 2 := add_lt_add h1 h2
-    simp 
     calc
       |f x - g x - (fl - gl)| 
-        = |(f x - fl) - (g x - gl)| := by ring
+        = |(f x - fl) - (g x - gl)| := by ring_nf
       _ ≤ |f x - fl| + |g x - gl| := abs_sub (f x - fl) (g x - gl)
       _ < ε/2 + ε/2  := hsum
       _ = ε := by ring
+
+theorem f_g_f_times_g_cont_at_c 
+  (f : ℝ → ℝ) 
+  (g : ℝ → ℝ) 
+  (f_c : f_is_continuous_at_a f c)
+  (g_c : f_is_continuous_at_a g c) :
+  f_is_continuous_at_a (fun x => f x * g x) c := by
+    let fpg : ℝ → ℝ := fun x => f x * g x
+    unfold f_is_continuous_at_a at f_c
+    unfold f_is_continuous_at_a at g_c
+    obtain ⟨fl, f_lim⟩ := f_c
+    obtain ⟨gl, g_lim⟩ := g_c
+    unfold f_is_continuous_at_a 
+    use fl * gl
+    unfold ε_δ_limit
+    intro ε ε_pos 
+    unfold ε_δ_limit at f_lim
+    unfold ε_δ_limit at g_lim
+    let εfl := ε / (2 * |fl| + 1)
+    let εgl := ε / (2 * |gl| + 1)
+    have εfl_pos :  εfl > 0 := by positivity
+    have εgl_pos :  εgl > 0 := by positivity
+    obtain ⟨δf, hf⟩ := f_lim εfl εfl_pos  
+    obtain ⟨δg, hg⟩ := g_lim εgl εgl_pos 
+    let δ := min δf δg
+    use δ 
+    intro x x_in_R x_ne_c_diff_le_δ 
+    obtain ⟨x_ne_c, diff_le_δ⟩ := x_ne_c_diff_le_δ
+    have hδ := lt_min_iff.1 diff_le_δ
+    have hδf := hδ.1
+    have hδg := hδ.2
+    have h1 : |f x - fl| < ε / (2 * |fl| + 1) := hf x trivial ⟨x_ne_c, hδf⟩
+    have h2 : |g x - gl| < ε / (2 * |gl| + 1) := hg x trivial ⟨x_ne_c, hδg⟩
+    have hsum : |f x - fl| + |g x - gl| < ε / (2 * |fl| + 1) + ε / (2 * |gl| + 1)   := add_lt_add h1 h2
+    simp 
+    calc
+      _ = |f x * g x - fl * gl + (f x)*gl - (f x)*gl| := by ring_nf
+      _ = |gl*(f x - fl) + (f x)*(g x -gl)| := by ring_nf
+      _ ≤ |gl*(f x - fl)| + |(f x)*(g x - gl)| :=
+        abs_add_le (gl*(f x - fl)) ((f x)*(g x - gl))
+      _ = |gl| * |(f x - fl)| + |(f x) * (g x - gl)| := by
+        rw [abs_mul gl (f x - fl), abs_mul (f x) (g x - gl)]
+      _ = |gl| * |f x - fl| + |f x| * |g x - gl| := by
+        rw [abs_mul (f x) (g x - gl)]
+    sorry 
+
