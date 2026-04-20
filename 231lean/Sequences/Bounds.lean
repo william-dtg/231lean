f0a34472a9d452bb14c6da41aee8c92148748028
wjkm <wjkm@mcknights.co.nz>
2026-04-21 08:46:31 +1200

    feat: define bounded below with constant example

--- 231lean/Sequences/Bounds.lean
+++ 231lean/Sequences/Bounds.lean
@@ -3,8 +3,15 @@ import «231lean».Sequences.Convergence
 def bounded_above (b : ℝ) (a : ℕ → ℝ) :=
   ∀ n, a n ≤ b
 
+def bounded_below (b : ℝ) (a : ℕ → ℝ) :=
+  ∀ n, b ≤ a n
+
 example (a : ℕ → ℝ) (h : ∀ n : ℕ, a n = a 0) :
     bounded_above (a 0) a := by
-      unfold bounded_above
       intro n 
       rw[h]
+
+example (a : ℕ → ℝ) (h : ∀ n : ℕ, a n = a 0) :
+    bounded_below (a 0) a := by
+      intro n 
+      rw[← h]
