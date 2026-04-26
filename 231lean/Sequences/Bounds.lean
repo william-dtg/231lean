0b1f3f2fcd04246dfa6ebdbc9ca14e29661c1e03
wjkm <wjkm@mcknights.co.nz>
2026-04-27 09:06:30 +1200

    feat: define bounded with example

--- 231lean/Sequences/Bounds.lean
+++ 231lean/Sequences/Bounds.lean
@@ -1,17 +1,25 @@
 import «231lean».Sequences.Convergence
 
-def bounded_above (b : ℝ) (a : ℕ → ℝ) :=
+def bounded_above (a : ℕ → ℝ) (b : ℝ) :=
   ∀ n, a n ≤ b
 
-def bounded_below (b : ℝ) (a : ℕ → ℝ) :=
+def bounded_below (a : ℕ → ℝ) (b : ℝ) :=
   ∀ n, b ≤ a n
 
-example (a : ℕ → ℝ) (h : ∀ n : ℕ, a n = a 0) :
-    bounded_above (a 0) a := by
+def bounded (a : ℕ → ℝ) :=
+  ∃ above, ∃ below, bounded_above a above ∧ bounded_below a below
+
+lemma const_bounded_above (a : ℕ → ℝ) (h : ∀ n : ℕ, a n = a 0) :
+    bounded_above a (a 0) := by
       intro n 
       rw[h]
 
-example (a : ℕ → ℝ) (h : ∀ n : ℕ, a n = a 0) :
-    bounded_below (a 0) a := by
+lemma const_bounded_below (a : ℕ → ℝ) (h : ∀ n : ℕ, a n = a 0) :
+    bounded_below  a (a 0) := by
       intro n 
       rw[← h]
+
+theorem const_bounded (a : ℕ → ℝ) (h : ∀ n : ℕ, a n = a 0) :
+    bounded a := by 
+      use (a 0), (a 0)
+      exact ⟨const_bounded_above a h, const_bounded_below a h⟩ 
