2eb588fd541a379919d708054e82299e76a7d13a
wjkm <wjkm@mcknights.co.nz>
2026-04-28 12:56:21 +1200

    feat: define sup and inf with example

--- 231lean/Sequences/Bounds.lean
+++ 231lean/Sequences/Bounds.lean
@@ -9,6 +9,12 @@ def bounded_below (a : ℕ → ℝ) (b : ℝ) :=
 def bounded (a : ℕ → ℝ) :=
   ∃ above, ∃ below, bounded_above a above ∧ bounded_below a below
 
+def supremum (a : ℕ → ℝ) (s : ℝ) :=
+  bounded_above a s ∧ ∀ b, bounded_above a b → s ≤ b
+
+def infimum (a : ℕ → ℝ) (i : ℝ) :=
+  bounded_below a i ∧ ∀ b, bounded_below a b → b ≤ i
+
 lemma const_bounded_above (a : ℕ → ℝ) (h : ∀ n : ℕ, a n = a 0) :
     bounded_above a (a 0) := by
       intro n 
@@ -23,3 +29,22 @@ theorem const_bounded (a : ℕ → ℝ) (h : ∀ n : ℕ, a n = a 0) :
     bounded a := by 
       use (a 0), (a 0)
       exact ⟨const_bounded_above a h, const_bounded_below a h⟩ 
+
+theorem const_sup (a : ℕ → ℝ) (h : ∀ n : ℕ, a n = a 0) :
+    supremum a (a 0) := by 
+      unfold supremum
+      constructor
+      · exact const_bounded_above a h
+      · intro b b_bounds
+        unfold bounded_above at b_bounds
+        exact b_bounds 0
+
+theorem const_inf (a : ℕ → ℝ) (h : ∀ n : ℕ, a n = a 0) :
+    infimum a (a 0) := by 
+      unfold infimum
+      constructor
+      · exact const_bounded_below a h
+      · intro b b_bounds
+        unfold bounded_below at b_bounds
+        exact b_bounds 0
+
