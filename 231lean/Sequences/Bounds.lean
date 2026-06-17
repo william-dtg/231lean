3a0dfd5c64ad18797ea5b60d374c8e153a15bc77
wjkm <wjkm@mcknights.co.nz>
2026-06-17 14:40:34 +1200

    feat: define cauchy and prove constant sequence is cauchy

--- 231lean/Sequences/Bounds.lean
+++ 231lean/Sequences/Bounds.lean
@@ -17,6 +17,8 @@ theorem sup_ex : ∀ a, ∀ b, bounded_above a b → ∃ s, supremum a s := sorr
 
 def increasing (a : ℕ → ℝ) := ∀ n : ℕ, a n ≤ a (n+1)
 
+def cauchy (a : ℕ → ℝ) := ∀ ε > 0, ∃ N : ℕ, ∀ n ≥ N, ∀ m ≥ N, |a n - a m| < ε  
+
 def infimum (a : ℕ → ℝ) (i : ℝ) :=
   bounded_below a i ∧ ∀ b, bounded_below a b → b ≤ i
 
@@ -60,10 +62,7 @@ lemma increasing_le (a : ℕ → ℝ) (inc : increasing a) :
       apply Nat.exists_eq_add_of_lt at N_lt_n
       obtain ⟨k, hk⟩ := N_lt_n
       induction k with
-      | zero =>
-        simp at hk
-        rw[hk]
-        exact inc N
+      | zero => grind
       | succ c ih =>
         have h2 : a (N + c + 1) ≤ a (N + (c+1) + 1) := by
           exact inc (N + c + 1)
@@ -73,10 +72,7 @@ lemma increasing_le (a : ℕ → ℝ) (inc : increasing a) :
         have ih' : a N ≤ a (N + c + 1) := by sorry
         linarith
 
-theorem increasing_bounded_converges 
-  (a : ℕ → ℝ)
-  (b : ℝ) 
-  (b_bounds : bounded_above a b)
+theorem increasing_bounded_converges (a : ℕ → ℝ) (b : ℝ) (b_bounds : bounded_above a b)
   (h : increasing a) :
     ∃ L, seq_converge a L := by
       obtain ⟨s, s_sup⟩ := sup_ex a b b_bounds
@@ -105,3 +101,10 @@ theorem increasing_bounded_converges
       · linarith
       · linarith
 
+theorem const_cauchy (a : ℕ → ℝ) (h : ∀ n : ℕ, a n = a 0) :
+    cauchy a := by
+      unfold cauchy
+      intro ε ε_pos
+      use 0
+      intro n _ m _
+      grind
